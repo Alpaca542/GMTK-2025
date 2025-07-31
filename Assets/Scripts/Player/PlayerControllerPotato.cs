0fb7d499136a1e03225091fe049ee88b411c3702
DataPotato2 <144252751+DataPotato2@users.noreply.github.com>
2025-07-31 07:36:27 -0400

    Update PlayerControllerPotato.cs

--- Assets/Scripts/Player/PlayerControllerPotato.cs
+++ Assets/Scripts/Player/PlayerControllerPotato.cs
@@ -7,12 +7,6 @@ public class PlayerControllerPotato : MonoBehaviour
     public float thrustForce = 10f;
     public float rotationSpeed = 200f;
     public float maxSpeed = 8f;
-    public float baseDrag = 1f;
-    public float airDrag = 3f;
-    public float inputSmoothing = 8f;
-    public float accelerationSmoothing = 12f;
-    public float momentumDrag = 0.98f;
-    public float additionalRotationSmoothing = 0.5f;
     [SerializeField] private float sideDrag;
     [SerializeField] private float sideTransfer;
     private float previousRot;
@@ -28,7 +22,6 @@ public class PlayerControllerPotato : MonoBehaviour
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.linearDamping = baseDrag;
     }
     void Update()
     {
@@ -49,27 +42,12 @@ public class PlayerControllerPotato : MonoBehaviour
     {
         targetInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-        /*
-        targetInput = Vector2.zero;
-        if (Input.GetKey(KeyCode.W)) targetInput += Vector2.up;
-        if (Input.GetKey(KeyCode.S)) targetInput += Vector2.down;
-        if (Input.GetKey(KeyCode.A)) targetInput += Vector2.left;
-        if (Input.GetKey(KeyCode.D)) targetInput += Vector2.right;
-        inputDirection = Vector2.Lerp(inputDirection, targetInput.normalized, inputSmoothing * Time.fixedDeltaTime);
-        *///smoothing input is for babies!!!
+
     }
 
     void HandleRotation()
     {
-        /*if (inputDirection.magnitude > 0.2f)
-        {
-            float targetAngle = Mathf.Atan2(inputDirection.y, inputDirection.x) * Mathf.Rad2Deg - 90f;
-            float currentAngle = transform.eulerAngles.z;
-            float angleDiff = Mathf.DeltaAngle(currentAngle, targetAngle);
-            float rotationAmount = Mathf.Sign(angleDiff) * Mathf.Min(Mathf.Abs(angleDiff), rotationSpeed * Time.fixedDeltaTime);
-            float smoothedRotation = Mathf.Lerp(0, rotationAmount, additionalRotationSmoothing * Time.fixedDeltaTime);
-            transform.Rotate(0, 0, smoothedRotation);
-        }*/
+
 
         rb.angularVelocity += -targetInput.x * rotationSpeed;
         float percentRotated = Mathf.Clamp01(Mathf.Abs(rb.rotation - previousRot)/90);
@@ -85,20 +63,7 @@ public class PlayerControllerPotato : MonoBehaviour
     void HandleMovement()
     {
         rb.AddForce(transform.up * thrustForce);
-        /*float inputMagnitude = inputDirection.magnitude;
-        targetThrust = Mathf.Clamp01(inputMagnitude);
-        currentThrust = Mathf.Lerp(currentThrust, targetThrust, accelerationSmoothing * Time.fixedDeltaTime);
-        if (currentThrust > 0.01f)
-        {
-            Vector2 thrustDirection = transform.up;
-            rb.AddForce(thrustForce * currentThrust * thrustDirection);
-            rb.linearDamping = baseDrag;
-        }
-        else
-        {
-            rb.linearDamping = airDrag;
-            rb.linearVelocity *= momentumDrag;
-        }*/
+
     }
 
     void ClampVelocity()
