3b06e6b9b6fa855c43ae8cb081580b9254848cc1
Alpaca <mihaililyin08@gmail.com>
2025-08-02 21:22:25 +0400

    new textures for everything

--- Assets/Scripts/UI/AnimEnder.cs
+++ Assets/Scripts/UI/AnimEnder.cs
@@ -5,12 +5,10 @@ public class AnimEnder : MonoBehaviour
 {
     public HandDrawing handDrawing;
     public GameObject player;
-    public GameObject fuel;
     public Animator anim;
     private void OnEnable()
     {
         handDrawing.AnimateDrawing(player, true);
-        fuel.SetActive(true);
         anim.enabled = false;
     }
 }
