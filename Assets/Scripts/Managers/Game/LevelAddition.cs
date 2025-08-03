9e25eb204f0a1568ba9676fbbec8e64728fa126c
Alpaca <mihaililyin08@gmail.com>
2025-08-03 20:39:23 +0400

    Update LevelAddition.cs

--- Assets/Scripts/Managers/Game/LevelAddition.cs
+++ Assets/Scripts/Managers/Game/LevelAddition.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using DG.Tweening;
-using UnityEditor.SearchService;
 
 public class LevelAddition : MonoBehaviour
 {
